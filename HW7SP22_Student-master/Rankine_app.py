ec3fd1e0b50594c287fed4094c49c37c5a56d6a6
Bryce D <102119818+Bryceadavis@users.noreply.github.com>
2022-03-27 19:57:12 -0500

    Update Rankine_app.py

--- HW7SP22_Student-master/Rankine_app.py
+++ HW7SP22_Student-master/Rankine_app.py
@@ -1,10 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtGui
 from Rankine_GUI import Ui_Form  # from the GUI file your created
-import rankineFile
-#oranges
-class main_window(qtw.QWidget, Ui_Form):
+import rankineFile as rankineFile
+
+class main_window(QWidget, Ui_Form):
     def __init__(self):
         """
         Constructor for the main window of the application.  This class inherits from QWidget and Ui_Form
@@ -14,6 +14,10 @@ class main_window(qtw.QWidget, Ui_Form):
         self.setWindowTitle('Rankine Cycle Calculator')  # set the window title
         # create a list of the labels on the main window
         self.Rankine = rankineFile.rankine()
+
+        self.label = [self.le_PHigh, self.le_PLow, self.le_TurbineInletCondition, self.le_TurbineEff, self.le_H1,
+                      self.le_H2, self.le_H3, self.le_H4, self.le_HeatAdded, self.le_PumpWork, self.le_Efficiency,
+                      self.le_TurbineWork]
         self.btn_Calculate.clicked.connect(self.Calculate)
         self.show()
     def Calculate(self):
@@ -23,67 +27,48 @@ class main_window(qtw.QWidget, Ui_Form):
         steam state.  Finally, output the results to the line edit widgets.
         :return:
         """
-        if self.rdo_Quality.clicked:
-            self.Rankine.p_high = float(self.le_PHigh.text())
-            self.Rankine.p_low = float(self.le_PLow.text())
-            self.Rankine.x = float(self.le_TurbineInletCondition.text())
-            self.Rankine.turbine_eff = float(self.le_TurbineEff.text())
 
-            self.Rankine.calc_efficiency()
-            state = self.Rankine
-            self.le_H1.setText("{:.2f}".format(self.Rankine.state1.h))
-            self.le_H2.setText("{:.2f}".format(self.Rankine.state2.h))
-            self.le_H3.setText("{:.2f}".format(self.Rankine.state3.h))
-            self.le_H4.setText("{:.2f}".format(self.Rankine.state4.h))
-            self.le_HeatAdded.setText("{:.2f}".format(self.Rankine.heat_added))
-            self.le_PumpWork.setText("{:.2f}".format(self.Rankine.pump_work))
-            self.le_Efficiency.setText("{:.2f}".format(self.Rankine.efficiency))
-            self.le_TurbineWork.setText("{:.2f}".format(self.Rankine.turbine_work))
+        self.Rankine.p_high = float(self.le_PHigh.text())*100
+        self.Rankine.p_low = float(self.le_PLow.text())*100
+        self.Rankine.eff_turbine = float(self.le_TurbineEff.text())
+        self.Rankine.quality = float(self.le_TurbineInletCondition.text()) if self.rdo_Quality.isChecked() else None
+        self.Rankine.t_high = float(self.le_TurbineInletCondition.text()) if self.rdo_THigh.isChecked() else None
+        self.Rankine.calc_efficiency()
 
-            self.lbl_SatPropHigh.setText(
-                "PSat = {:.2f} bar, TSat= {:.2f} C\nhf = {:.2f} kJ/kg , hg = {:.2f} kJ/kg\nsf= {:.2f} kJ/kgK, "
-                "sg= {:.2f} kJ/kgK\nvf= {:.4f} m^3/kg, vg= {:.2f} m^3/kg ".format(self.Rankine.p_high,
-                                                                                  self.Rankine.state1.T,
-                                                                                  self.Rankine.state1.hf,
-                                                                                  self.Rankine.state1.h,
-                                                                                  self.Rankine.state3.s,
-                                                                                  self.Rankine.state1.s,
-                                                                                  self.Rankine.state3.v,
-                                                                                  self.Rankine.state1.v))
-            self.lbl_SatPropLow.setText(
-                "PSat={:.2f} bar, TSat = {:.2f} C\nhf = {:.2f} kJ / kg, hg = {:.2f} kJ / kg\nsf = {:.2f} kJ / kgK, "
-                "sg = {:.2f} kJ / kgK\nvf = {:.4f} m ^ 3 / kg, vg = {:.2f} m ^ 3 / kg ".format(self.Rankine.p_low,
-                                                                                               self.Rankine.state2.T,
-                                                                                               self.Rankine.state2.hf,
-                                                                                               self.Rankine.state4.h,
-                                                                                               self.Rankine.state3.s,
-                                                                                               self.Rankine.state1.s,
-                                                                                               self.Rankine.state3.v,
-                                                                                               self.Rankine.state1.v
-                                                                                               ))
 
-            self.show()
-            return
+        self.le_H1.setText("{:.2f}".format(self.Rankine.state1.h))
+        self.le_H2.setText("{:.2f}".format(self.Rankine.state2.h))
+        self.le_H3.setText("{:.2f}".format(self.Rankine.state3.h))
+        self.le_H4.setText("{:.2f}".format(self.Rankine.state4.h))
+        self.le_HeatAdded.setText("{:.2f}".format(self.Rankine.heat_added))
+        self.le_PumpWork.setText("{:.2f}".format(self.Rankine.pump_work))
+        self.le_Efficiency.setText("{:.2f}".format(self.Rankine.efficiency))
+        self.le_TurbineWork.setText("{:.2f}".format(self.Rankine.turbine_work))
 
-        # elif self.rdo_THigh.clicked:
-        #     self.Rankine.p_high = float(self.le_PHigh.text()) if self.le_PHigh.isEnabled() else None
-        #     self.Rankine.p_low = float(self.le_PLow.text()) if self.le_PLow.isEnabled() else None
-        #     self.Rankine.efficiency = float(self.le_Efficiency.text()) if self.le_Efficiency.isEnabled() else None
-        #     self.Rankine.eff_turbine = float(
-        #         self.le_TurbineInletCondition.text()) if self.le_TurbineEff.isEnabled() else None
-        #
-        #     self.Rankine.calc_efficiency()
-        #     state = self.Rankine
-        #     self.le_H1.setText(str(round(self.Rankine.state1.h, 2)))
-        #     self.le_H2.setText(str(round(self.Rankine.state2.h, 2)))
-        #     self.le_H3.setText(str(round(self.Rankine.state3.h, 2)))
-        #     self.le_H4.setText(str(round(self.Rankine.state4.h, 2)))
-        #     self.le_HeatAdded.setText(str(round(self.Rankine.heat_added, 2)))
-        #     self.le_PumpWork.setText(str(round(self.Rankine.pump_work, 2)))
-        #     self.le_Efficiency.setText(str(round(self.Rankine.efficiency, 2)))
-        #     self.le_TurbineWork.setText(str(round(self.Rankine.turbine_work, 2)))
-        #     self.show()
-        #     return
+
+        self.lbl_SatPropHigh.setText(
+            "PSat = {:.2f} bar, TSat= {:.2f} C\nhf = {:.2f} kJ/kg , hg = {:.2f} kJ/kg\nsf= {:.2f} kJ/kgK, "
+            "sg= {:.2f} kJ/kgK\nvf= {:.4f} m^3/kg, vg= {:.2f} m^3/kg ".format(self.Rankine.p_high/100,
+                                                                              self.Rankine.state1.T,
+                                                                              self.Rankine.state1.hf,
+                                                                              self.Rankine.state1.hg,
+                                                                              self.Rankine.state1.sf,
+                                                                              self.Rankine.state1.sg,
+                                                                              self.Rankine.state1.vf,
+                                                                              self.Rankine.state1.vg))
+        self.lbl_SatPropLow.setText(
+            "PSat={:.2f} bar, TSat = {:.2f} C\nhf = {:.2f} kJ / kg, hg = {:.2f} kJ / kg\nsf = {:.2f} kJ / kgK, "
+            "sg = {:.2f} kJ / kgK\nvf = {:.4f} m ^ 3 / kg, vg = {:.2f} m ^ 3 / kg ".format(self.Rankine.p_low/100,
+                                                                                           self.Rankine.state2.T,
+                                                                                           self.Rankine.state2.hf,
+                                                                                           self.Rankine.state2.hg,
+                                                                                           self.Rankine.state2.sf,
+                                                                                           self.Rankine.state2.sg,
+                                                                                           self.Rankine.state2.vf,
+                                                                                           self.Rankine.state2.vg))
+
+
+        return
 
     def ExitApp(self):
         app.exit()
