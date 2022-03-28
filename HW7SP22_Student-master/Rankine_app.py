916544bdad61a30ff5dca1f51d4b667832d838b2
Bryce D <102119818+Bryceadavis@users.noreply.github.com>
2022-03-28 10:12:29 -0500

    Update Rankine_app.py

--- HW7SP22_Student-master/Rankine_app.py
+++ HW7SP22_Student-master/Rankine_app.py
@@ -1,20 +1,20 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5 import QtGui
-from Rankine_GUI import Ui_Form  # from the GUI file you created
+from PyQt5 import QtCore, QtGui
+from Rankine_GUI import Ui_Form  # from the GUI file your created
 import rankineFile as rankineFile
-#some code borrowed from Calc_state_app_stem.py
+#oranges
 class main_window(QWidget, Ui_Form):
     def __init__(self):
         """
         Constructor for the main window of the application.  This class inherits from QWidget and Ui_Form
         """
+        # some code borrowed from calc_state_app_stem
         super().__init__()  # run constructor of parent classes
         self.setupUi(self)  # run setupUi() (see Ui_Form)
         self.setWindowTitle('Rankine Cycle Calculator')  # set the window title
         # create a list of the labels on the main window
         self.Rankine = rankineFile.rankine()
-
         self.label = [self.le_PHigh, self.le_PLow, self.le_TurbineInletCondition, self.le_TurbineEff, self.le_H1,
                       self.le_H2, self.le_H3, self.le_H4, self.le_HeatAdded, self.le_PumpWork, self.le_Efficiency,
                       self.le_TurbineWork]
@@ -22,23 +22,24 @@ class main_window(QWidget, Ui_Form):
         self.show()
     def Calculate(self):
         """
-        Part 1: Here, we need to set the properties of the rankine object and calculate: the
-        enthalpies of each state,heat added,pump work, and overall cycle efficiency.
-        Finally, output the results to the line edit widgets.
-        Part 2: Fill in the Saturated Properties labels accordingly. Necessary adjustments made to Steam_work and Dr. Smay's Rankine file
-        (renamed: rankineFile and modified).
+        Here, we need to scan through the check boxes and ensure that only two are selected a defining properties
+        for calculating the state of the steam.  Then set the properties of the steam object and calculate the
+        steam state.  Finally, output the results to the line edit widgets.
         :return:
         """
-
-        self.Rankine.p_high = float(self.le_PHigh.text())*100 #these lines assign vals to rankine params
-        self.Rankine.p_low = float(self.le_PLow.text())*100 #bar to kPa
+        translate = QtCore.QCoreApplication.translate
+        self.Rankine.p_high = float(self.le_PHigh.text())*100  # these lines assign vals to rankine params
+        self.Rankine.p_low = float(self.le_PLow.text())*100  # bar to kpa
         self.Rankine.eff_turbine = float(self.le_TurbineEff.text())
         self.Rankine.quality = float(self.le_TurbineInletCondition.text()) if self.rdo_Quality.isChecked() else None
-        self.Rankine.t_high = float(self.le_TurbineInletCondition.text()) if self.rdo_THigh.isChecked() else None #doesn't work but it was a nice thought
-        self.Rankine.calc_efficiency() #necessary to get access to the states in the lines below
+        self.Rankine.t_high = float(self.le_TurbineInletCondition.text()) if self.rdo_THigh.isChecked() else None
+        if self.rdo_Quality.isChecked():
+            self.lbl_TurbineInletCondition.setText(translate("Form", "Turbine Inlet, x:"))
+        if self.rdo_THigh.isChecked():
+            self.lbl_TurbineInletCondition.setText(translate("Form", "Turbine Inlet, T_high:"))
+        self.Rankine.calc_efficiency()  # necessary to get access to the states in the lines below
 
-
-        self.le_H1.setText("{:.2f}".format(self.Rankine.state1.h)) #fills in the unchangeable line edits at bottom according to calculations made from above inputs
+        self.le_H1.setText("{:.2f}".format(self.Rankine.state1.h))  #fills in the unchangeable line edits at bottom according to calculations made from above inputs
         self.le_H2.setText("{:.2f}".format(self.Rankine.state2.h))
         self.le_H3.setText("{:.2f}".format(self.Rankine.state3.h))
         self.le_H4.setText("{:.2f}".format(self.Rankine.state4.h))
@@ -54,25 +55,24 @@ class main_window(QWidget, Ui_Form):
                                                                               self.Rankine.state1.T,
                                                                               self.Rankine.state1.hf,
                                                                               self.Rankine.state1.hg,
-                                                                              self.Rankine.state1.sf,
+                                                                              self.Rankine.state2.sf,
                                                                               self.Rankine.state1.sg,
-                                                                              self.Rankine.state1.vf,
-                                                                              self.Rankine.state1.vg))   #Bryce made adjustments to rankine and steam files for these labels to work
+                                                                              self.Rankine.state3.vf,
+                                                                              self.Rankine.state1.vg))
         self.lbl_SatPropLow.setText(
             "PSat={:.2f} bar, TSat = {:.2f} C\nhf = {:.2f} kJ / kg, hg = {:.2f} kJ / kg\nsf = {:.2f} kJ / kgK, "
             "sg = {:.2f} kJ / kgK\nvf = {:.4f} m ^ 3 / kg, vg = {:.2f} m ^ 3 / kg ".format(self.Rankine.p_low/100,
                                                                                            self.Rankine.state2.T,
                                                                                            self.Rankine.state2.hf,
-                                                                                           self.Rankine.state2.hg,
-                                                                                           self.Rankine.state2.sf,
-                                                                                           self.Rankine.state2.sg,
-                                                                                           self.Rankine.state2.vf,
-                                                                                           self.Rankine.state2.vg))
-
+                                                                                           self.Rankine.state4.hg,
+                                                                                           self.Rankine.state3.sf,
+                                                                                           self.Rankine.state1.sg,
+                                                                                           self.Rankine.state3.vf,
+                                                                                           self.Rankine.state1.vg))  # Bryce made adjustments to rankine and steam files for these labels to work
 
         return
 
-    def ExitApp(self): #allows app to be exited
+    def ExitApp(self):
         app.exit()
 
 if __name__ == "__main__":
